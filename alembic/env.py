ed3e657465d4548b91778c565e0fb3cf6cf1ab29
fahiiim <fahimsarker0805@gmail.com>
2026-08-30 15:55:32 +0600

    Test Cases

--- alembic/env.py
+++ alembic/env.py
@@ -3,15 +3,14 @@ from __future__ import annotations
 import asyncio
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from alembic import context
 from app.core.config import get_settings
-from app.db.base import Base, _async_database_url
 from app.db import models  # noqa: F401
-
+from app.db.base import Base, _async_database_url
 
 config = context.config
 if config.config_file_name is not None:
